9801a19cbf826ad494897dc301a087f3e58dd993
Luigi Cruz <alainluigicruz@gmail.com>
2021-03-18 03:11:01 +0800

    wip

--- src/components/button/styled.ts
+++ src/components/button/styled.ts
@@ -29,10 +29,6 @@ export const StyledButton = styled.button<StyledButtonProps>`
     display: flex;
     align-items: center;
     justify-content: center;
-    /* Add margin in case of loading or icon */
-    & > *:nth-child(1) {
-        margin-left: ${pr => pr.withText ? 7 : 0}px;
-    }
     font-size: 15px;
     border: none;
     cursor: pointer;
@@ -40,15 +36,6 @@ export const StyledButton = styled.button<StyledButtonProps>`
     padding: 0 ${ pr => sidePaddings[pr.size]}px;
     height: ${ pr => heights[pr.size]}px;
     color: '#FFFFFF';
-    ${ pr => pr.disabled ? `
-        background-color: #a6a6a6;
-        color: #5e5e5e;
-        cursor: not-allowed;
-        &:hover {
-            background-color: #a6a6a6 !important;
-            color: #5e5e5e !important;
-        }
-    ` : ''}
     border-radius: 0;
     outline: none;
     &:focus {
